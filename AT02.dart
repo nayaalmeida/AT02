f88c265a4ee39aa13e5219ecb95b4ea6c75def28
Naya Almeida <nayagomess@outlook.com>
2023-08-07 20:47:41 -0300

    Métodos Getter e Setters

--- AT02.dart
+++ AT02.dart
@@ -1,5 +1,9 @@
 class Teste{
   var estudante, nota;
 
+  String getEstudante() => this.estudante;
+  void setEstudante(String e){this.estudante = e;}
 
+  double getNota() => this.nota;
+  void setNota(double n){this.nota = n;}
 }
